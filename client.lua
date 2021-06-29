83c03ee2740b71d0f1221dfc5757d4f5bb91f17d
Linden <65407488+thelindat@users.noreply.github.com>
2021-06-30 08:38:38 +1000

    Update to latest

--- client.lua
+++ client.lua
@@ -1,6 +1,9 @@
 local Disable = {}
 Citizen.CreateThread(function()
 	while true do
+		HideHudComponentThisFrame(14)
+		HideHudComponentThisFrame(19)
+		HideHudComponentThisFrame(20)
 		if next(Disable) then
 			for k,v in pairs(Disable) do
 				DisableControlAction(0, v, true)
@@ -12,9 +15,14 @@ end)
 
 -- Setup different sets of keys to disable depending on active variables
 -- Either trigger ESX.SetPlayerData('key', state) or add an event to set keys to disable like below
+-- todo: make a general disable list
 local Keys = {
-	handsup = {23, 24, 25, 37, 45, 166, 167, 168, 170, 257, 263, 288, 289},
+	always = {199}
+	handsup = {23, 24, 25, 45, 166, 167, 168, 170, 257, 263, 288},
 }
+for k,v in pairs(Keys['always']) do
+	Disable[k] = v
+end
 
 OnPlayerData = function(key, val)
 	if key == 'handsup' then
@@ -30,120 +38,71 @@ OnPlayerData = function(key, val)
 	end
 end
 
-
 local Threads = {}
-
--- Create a thread that triggers an event when state changes to true/fale
-local BoolThread = function(name, interval, action, args)
-	local state, current = false
-	local args = table.unpack(args)
+local CreateThread = function(name, interval, action)
+	local self = {}
+	self.name = name	self.interval = interval	self.action = action	self.current = false
 	Citizen.CreateThread(function()
-		while ESX.PlayerLoaded and Threads[name] do
-			Citizen.Wait(interval)
-			if args == 'playerped' then args = ESX.PlayerData.ped end
-			local result = action(args)
-			if state and (not result or result == 0) then
-				state = false
-				TriggerEvent(name..'Thread', state)
-			elseif not state and result > 0 then
-				state, current = true, result
-				TriggerEvent(name..'Thread', state, result)
+		while self.name do
+			local result = self.action()
+			if result == 0 or nil then result = false end
+			if result ~= self.current then
+				self.current = result
+				if not result then
+					TriggerEvent('thread:'..self.name, false)
+					self.interval = self.interval
+				else
+					TriggerThread(self.name, result)
+					self.interval = self.interval
+				end
+			end
+			Citizen.Wait(self.interval)
+		end
+		TriggerEvent('thread:'..self.name, false)
+		Threads[name] = nil
+	end)
+	return self
+end
+
+local ToggleThread = function(name, interval, action)
+	if Threads[name] then
+		if interval then Threads[name].interval = interval else Threads[name].name = nil end
+	else
+		Threads[name] = CreateThread(name, interval, action)
+	end
+end
+
+TriggerThread = function(name, result)
+	Citizen.CreateThread(function()
+		local data = {}
+		if name == 'vehicle' then
+			data.vehicle = result
+			data.class = GetVehicleClass(result)
+			local seat = GetPedInVehicleSeat(result, -1)
+			if seat == ESX.PlayerData.ped then data.driver = true else data.driver = false end
+		end
+		TriggerEvent('thread:'..name, data)
+	end)
+end
+
+ToggleThread('vehicle', 600, function()
+	return GetVehiclePedIsIn(ESX.PlayerData.ped, false)
+end)
+
+
+
+-- Then you can throw in an event wherever you want when entering/exiting a vehicle
+
+AddEventHandler('thread:vehicle', function(data)
+	if data and vehicle ~= data.vehicle then
+		vehicle = data.vehicle
+		vehicleClass = data.class
+		if vehicleClass ~= 21 and vehicleClass ~= 13 then
+			if data.driver and vehicleClass ~= 15 and vehicleClass ~= 16 then
+				driving = true
 			end
 		end
-		TriggerEvent(name..'Thread', false)
-	end)
-end
-
--- Create a thread that triggers an event when the result changes
-local UpdateThread = function(name, interval, action, args)
-	local current
-	local args = table.unpack(args)
-	Citizen.CreateThread(function()
-		while ESX.PlayerLoaded and Threads[name] do
-			Citizen.Wait(interval)
-			if args == 'playerped' then args = ESX.PlayerData.ped end
-			local result = action(args)
-			if result ~= current then
-				current = result
-				TriggerEvent(name..'Thread', result)
-			end
-		end
-		TriggerEvent(name..'Thread', false)
-	end)
-end
-
--- Create a thread that triggers an event at a set interval
-local Thread = function(name, interval, action, args)
-	local args = table.unpack(args)
-	Citizen.CreateThread(function()
-		while ESX.PlayerLoaded and Threads[name] do
-			Citizen.Wait(interval)
-			if args == 'playerped' then args = ESX.PlayerData.ped end
-			local result = action(args)
-			TriggerEvent(name..'Thread', result)
-		end
-		TriggerEvent(name..'Thread', false)
-	end)
-end
-
-
-local ToggleThread = function(name, type, interval, action, args)
-	if Threads[name] then Threads[name] = nil
-	elseif interval then Threads[name] = true
-		if type == 'bool' then
-			BoolThread(name, interval, action, args)
-		elseif type == 'update' then
-			UpdateThread(name, interval, action, args)
-		else
-			Thread(name, interval, action, args)
-		end
-	end
-end
-
-ToggleThread('Vehicle', 'bool', 500, GetVehiclePedIsUsing, {'playerped'})
-ToggleThread('Armour', 'update', 1000, GetPedArmour, {'playerped'})
-ToggleThread('Coords', '', 100, GetEntityCoords, {'playerped'})
-
-
-
-
-
--- Example usage for toggling a thread
-local on = true
-RegisterCommand('printcoords', function()
-	if on then
-		ToggleThread('Coords')
 	else
-		ToggleThread('Coords', '', 100, GetEntityCoords, {'playerped'})
+		vehicle, vehicleClass = nil, nil
 	end
-	on = not on
-end)
-
-
--- Example events
-
--- Bool
-AddEventHandler('VehicleThread', function(state, result)
-	inVehicle = state
-	if not inVehicle then
-		print('Not driving a vehicle')
-	else
-		while inVehicle do
-			local speed = math.floor(GetEntitySpeed(result) * 3.6)
-			print('Driving '..speed..' km/h')
-			Citizen.Wait(100)
-		end
-	end
-end)
-
--- Update
-AddEventHandler('ArmourThread', function(result)
-	if result then
-		print('Armour is currently '..result)
-	end
-end)
-
--- Default
-AddEventHandler('CoordsThread', function(result)
-	print(result)
 end)
