dc92ebfc18577de77d2f09182d4d52f1b6fe55a4
Linden <65407488+thelindat@users.noreply.github.com>
2021-06-30 17:11:08 +1000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,5 +5,12 @@ description 'https://github.com/thelindat/linden_threads'
 
 client_scripts {
 	'@es_extended/imports.lua',
-	'client.lua'
+}
+
+shared_scripts {
+	'shared/*.lua'
+}
+
+files {
+	'shared/interval.lua',	-- Load in any resource with	shared_script '@linden_thread/shared/interval.lua
 }
